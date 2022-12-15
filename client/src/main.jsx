62e424cd882da8fa377b935d3322fbfc54a9e8e2
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2022-12-15 04:14:07 -0500

    Almost finished setting up Sidebar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 import App from "./App";
 import "./main.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <ThirdwebProvider desiredChainId={ChainId.Goerli}>
