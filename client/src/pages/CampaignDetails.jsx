53e5610e1204e1962c7dbb691e92e78b92f7bf4d
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2022-12-24 04:27:56 -0500

    Update CampaignDetails.jsx

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { ethers } from "ethers";
+
+import { useStateContext } from "../context";
+import { CustomButton } from "../components";
+import { calculateBarPercentage, daysLeft } from "../utils";
+import { thirdweb } from "../assets";
 
 const CampaignDetails = () => {
-  return <div>CampaignDetails</div>;
+  const { state } = useLocation();
+  const { getDonations, contract, address } = useStateContext();
+
+  const [loading, setLoading] = useState(false);
+  const [amount, setAmount] = useState("");
+  const [donators, setDonators] = useState([]);
+
+  const remainingDays = daysLeft(state.deadline);
+
+  return (
+    <div>
+      {loading && "Loaading..."}
+      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+        <div className="flex-1 flex-col">
+          <img
+            src={state.image}
+            alt="campaign-image"
+            className="w-full h-[410px] object-cover rounded-xl"
+          />
+          <div className="relative w-full h-[5px] bg-[3a3a43] mt-2 ">
+            <div
+              className="absolute h-full bg-[#4acd8d]"
+              style={{
+                width: `${calculateBarPercentage(
+                  state.target,
+                  state.amountCollected
+                )}%`,
+                maxWidth: "100%",
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CampaignDetails;
